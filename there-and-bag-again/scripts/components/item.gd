3a17d56e0ee17d1025a35e749bbecd1eff97c9a4
Roqueasoto <roquealbertosoto@gmail.com>
2026-01-17 15:19:46 -0500

    More resource things to remove

--- there-and-bag-again/scripts/components/item.gd
+++ there-and-bag-again/scripts/components/item.gd
@@ -6,8 +6,6 @@ extends Node2D
 @export var layout: Array   # 2D Array of booleans, which determine item shape.
 @export var properties: Array[Utils.property]
 
-var info: ItemInfo
-
 # Need a way to generate item from item infos
 func _ready() -> void:
 	# Load icon from path into

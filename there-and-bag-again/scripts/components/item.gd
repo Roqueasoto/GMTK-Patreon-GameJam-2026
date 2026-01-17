0aa9ae9e79c5c76d31318803891b6d71f0636a94
Roqueasoto <roquealbertosoto@gmail.com>
2026-01-17 15:19:23 -0500

    Remove Unecessary Resource stuff

--- there-and-bag-again/scripts/components/item.gd
+++ there-and-bag-again/scripts/components/item.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 @export var sprite: Sprite2D
 @export var collider: CollisionShape2D
+@export var layout: Array   # 2D Array of booleans, which determine item shape.
+@export var properties: Array[Utils.property]
+
 var info: ItemInfo
 
 # Need a way to generate item from item infos
 func _ready() -> void:
-	sprite.texture = info.texture
-	collider.shape = info.collision_shape
 	# Load icon from path into
 	pass
--- there-and-bag-again/scripts/framework/item_info.gd
+++
@@ -1,8 +0,0 @@
-class_name ItemInfo
-extends Resource
-
-@export var name: String    # Unique
-@export var texture: Texture2D
-@export var collision_shape: ConvexPolygonShape2D
-@export var layout: Array   # 2D Array of booleans, which determine item shape.
-@export var properties: Array[Utils.property]

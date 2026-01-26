69da798bb69f9d0005de7b4fef1e9c13b5c5eb4e
Roqueasoto <roquealbertosoto@gmail.com>
2026-01-26 03:31:28 -0500

    Additional bug fixes
    Exports of the game to Mac and PC (ignored) for testing, additional bug fixes around resource paths, which are not discoverable after export (and thus we need to use resource groups). Also created unique ids and names for every item.

--- there-and-bag-again/scripts/components/bag.gd
+++ there-and-bag-again/scripts/components/bag.gd
@@ -2,7 +2,7 @@ class_name Bag
 extends Control
 
 const ITEM_SCENE = preload("res://scenes/components/items/item.tscn")
-const ITEMS_PATH = "res://resources/item_data/"
+@export var all_items: ResourceGroup = preload("res://resources/groups/all_items.tres")
 
 @onready var board: Board = $Board
 
@@ -11,21 +11,8 @@ func _ready() -> void:
 	print("Bag Initialized. Registry: ", board.item_registry.keys())
 
 func _load_items_into_registry() -> void:
-	var dir = DirAccess.open(ITEMS_PATH)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		
-		while file_name != "":
-			if not dir.current_is_dir() and file_name.ends_with(".tres"):
-				var resource = load(ITEMS_PATH + file_name)
-				# Use the file name (without extension) as the registry key
-				var key = file_name.get_basename()
-				board.item_registry[key] = resource
-			
-			file_name = dir.get_next()
-	else:
-		push_error("Failed to access path: " + ITEMS_PATH)
+	for item : ItemData in all_items.load_all():
+		board.item_registry[item.id] = item
 
 func _spawn(data: Resource, grid_pos: Vector2i) -> void:
 	var item = ITEM_SCENE.instantiate()
